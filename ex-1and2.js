af01820e9f5211b98c6b2970328a571086434bce
Kittikhong Chanthakorn <kityakuza@gmail.com>
2024-10-21 00:32:35 +0700

    Answer Exercise TechUp

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let peopleName = "Karter";
+let peopleLastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 //Exercise #2: Variable Reassignment
 //Start coding here
+const restaurentName = "Chef's Table by Chef James"
+const billingNetPrice = 15000;
+const restaurentActive = 1500;
